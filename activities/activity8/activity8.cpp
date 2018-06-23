d7d0fe485d8b7e1dabc186912cbf217a8386f5f8
hunterbowman <40217936+hunterbowman@users.noreply.github.com>
2018-06-22 22:47:40 -0700

    Activity 8
    I did activity 8 and i did some rearranging.

---
+++ activities/activity8/activity8.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	
+	char word1[25];
+	int i;
+	
+	cout << "Enter a word: ";
+	cin >> word1;
+	
+	for(i=0; i < 5; i++) {
+		
+		cout << word1 <<endl;
+		
+	}
+	
+	return 0;
+	
+}
