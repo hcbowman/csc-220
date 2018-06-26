7bb0002cbe2b9a4f7344644ea48b26a5d0159822
hunterbowman <40217936+hunterbowman@users.noreply.github.com>
2018-06-25 21:52:26 -0700

    midterm practice
    Finished the midterm practice, and started pract 7.

--- quizes_tests/midterm/midtermpractice/code/midpractice.c
+++ quizes_tests/midterm/midtermpractice/code/midpractice.c
@@ -12,7 +12,7 @@ int multiple(int a, int b)
 int main()
 {
    int a, *b;
-   int ii;
+   int ii, y;
 
 
    printf("Enter a number\n");
@@ -21,13 +21,20 @@ int main()
    b = (int *) malloc(a);
 
    /* Fill array b using pointer arithmetic */
-   for (ii=0; ii<a; ii++)
-	   b+ii = multiple(a,ii+1);
-
+   for (ii=0; ii<a; ii++) {
+	   y = multiple(a,ii);
+	   *(b + ii) = y;
+	   
+	}
+   
+   
+   
    /* Print array b using array indexing */
    printf ("The multiples of %d are : ",a);
-   for (ii=0; ii<a; ii++)
+   for (ii=0; ii<a; ii++) {
 	   printf("%d ",b[ii]);
-
+	   
+   }
+   
    return (0);
 }
