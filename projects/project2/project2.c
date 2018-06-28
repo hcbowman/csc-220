4a139e7ed4dd5b9b95c13acb643fd3fa6349ce22
hunterbowman <40217936+hunterbowman@users.noreply.github.com>
2018-06-27 23:22:41 -0700

    Activity9 and started project2

---
+++ projects/project2/project2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+
+
+/* Create a new matrix */
+matrix_t * new_matrix(int rows, int cols)
+	
+	
+/* Neatly print the matrix */
+void print_matrix(const matrix_t * matrix)
+	
+	
+/* Get/Set the value at row, col */
+int get_value(const matrix_t * matrix, int row, int col)
+	
+void set_value(matrix_t * matrix, int row, int col, int value)
+	
+	
+/* Return the sum of two matrices */
+matrix_t * add(const matrix_t * m1, const matrix_t * m2)
+	
+	
+/* Return the transpose of a matrix */
+matrix_t * transpose(const matrix_t * matrix)
+	
+	
+	
+
+int main() {
+	
+	
+	
+}
